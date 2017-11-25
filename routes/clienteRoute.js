841dd3aa3c26c01e65d655500a1c15dc1f90cf5a
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-11-25 16:49:44 -0200

    rolesMiddleware: added roles middleware and set it to the router
    We added the role based middleware that verifies if the user can
    access certain resource.

--- routes/clienteRoute.js
+++ routes/clienteRoute.js
@@ -1,13 +1,14 @@
 const route = require("express").Router();
 const clienteController = require("../controllers/clienteController");
+const rolesMiddleware = require('../middleware/roleMiddleware');
 
-route.post("/clientes", clienteController.createCliente);
-route.get("/clientes", clienteController.getClientes);
-route.get("/clientes/:id", clienteController.getOneClient);
-route.put("/clientes/:id", clienteController.updateCliente);
+route.post("/clientes", rolesMiddleware(['cadastro', 'administrador']), clienteController.createCliente);
+route.get("/clientes", rolesMiddleware(['all']), clienteController.getClientes);
+route.get("/clientes/:id", rolesMiddleware(['all']), clienteController.getOneClient);
+route.put("/clientes/:id", rolesMiddleware(['cadastro', 'administrador']), clienteController.updateCliente);
 
-route.delete("/clientes/:clienteId/enderecos/:enderecoId", clienteController.removeEndereco);
-route.delete("/clientes/:clienteId/contatos/:contatoId", clienteController.removeContato);
+route.delete("/clientes/:clienteId/enderecos/:enderecoId", rolesMiddleware(['cadastro', 'administrador']), clienteController.removeEndereco);
+route.delete("/clientes/:clienteId/contatos/:contatoId", rolesMiddleware(['cadastro', 'administrador']), clienteController.removeContato);
 
 
 module.exports = route;
