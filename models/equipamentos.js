b801fd72232ac71194d10a51ca3f7a2975fa7f3c
alexandrejuk <ale_santos.soares@hotmail.com>
2018-05-28 15:36:45 -0300

    equipamentos: criado model para equipamentos

---
+++ models/equipamentos.js
@@ -0,0 +1,24 @@
+const { Schema } = require("mongoose");
+const timestamps = require('mongoose-timestamp');
+const dbConnection = require('../databaseConnection');
+const userAudit = require('mongoose-useraudit-plugin');
+
+const pecaSchema  = new Schema({
+  descricao: { type: String }, 
+  valor: { type: Number }, 
+})
+
+const produtoSchema = new Schema({
+  modelo: { type: String, required: [true, "Entre com modelo do produto"] },
+  descricao: { type: String, required: [true, "Entre com modelo do produto"] },
+  marca: { type: String, required: [true, "Entre com modelo do produto"] },
+  categoria: { type: String, enum: ["acessório","catraca", "relógio", "peça", "software"], required: [true, "Entre com a categoria!"] },
+  pecas: [pecaSchema],
+  valor: { type: Number, required: [true, "Entre com o valor do produto"], default: 0 },
+  imagemURL: { type: String, required: [true, "Entre com a imagem valor do produto"], default: null },
+}, { versionKey: false });
+
+produtoSchema.plugin(timestamps);
+produtoSchema.plugin(userAudit);
+
+module.exports = dbConnection.model("produtos", produtoSchema);
