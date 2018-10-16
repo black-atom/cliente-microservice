5ab54a0a0e34aac00454c62a920db3f99d679b1a
alexandrejuk <ale_santos.soares@hotmail.com>
2018-10-16 16:42:06 -0300

    component: create stock

--- routes/estoqueRoute.js
+++ routes/estoqueRoute.js
@@ -3,5 +3,6 @@ const estoqueController = require("../controllers/estoqueController");
 const rolesMiddleware = require('../middleware/roleMiddleware');
 
 route.get("/estoque", rolesMiddleware(['all']), estoqueController.getAllTransactions);
-
+route.get("/estoque/:id", rolesMiddleware(['all']), estoqueController.getEquipamentsToAddSerialNumber);
+route.post("/estoque", rolesMiddleware(['all']), estoqueController.saveProductsSerial);
 module.exports = route;
